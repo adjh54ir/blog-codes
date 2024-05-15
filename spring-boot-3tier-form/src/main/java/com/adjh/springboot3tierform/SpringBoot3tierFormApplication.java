e15809d847cd2c3b3fd1ed29b4126e6a09d2850c
jonghoon_lee <adjh54ir@gmail.com>
2024-05-15 19:09:34 +0900

    spring-boot-3tier Repository 구성 1차 완료

---
+++ spring-boot-3tier-form/src/main/java/com/adjh/springboot3tierform/SpringBoot3tierFormApplication.java
@@ -0,0 +1,13 @@
+package com.adjh.springboot3tierform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3tierFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3tierFormApplication.class, args);
+    }
+
+}
---
+++ spring-boot-3tier-form/src/main/java/com/adjh/springboot3tierform/dao/TemplateDao.java
@@ -0,0 +1,11 @@
+package com.adjh.springboot3tierform.dao;
+
+/**
+ * Please explain the class!!
+ *
+ * @author : jonghoon
+ * @fileName : TemplateDao
+ * @since : 5/15/24
+ */
+public interface TemplateDao {
+}
