4106986718c372c93f082ac109a93052a45147c1
jonghoon_lee <adjh54ir@gmail.com>
2024-06-08 16:44:19 +0900

    spring-boot-webflux 구성중

---
+++ spring-boot-webflux/src/test/java/com/adjh/springbootwebflux/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adjh.springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
