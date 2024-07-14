01637cbf4e6bfdd5d89648203650d46b96eae732
jonghoon_lee <adjh54ir@gmail.com>
2024-07-14 14:29:31 +0900

    spring-boot-async 초기 환경 구성

---
+++ spring-boot-async/src/main/java/com/adjh/springbootasync/controller/AsyncController.java
@@ -0,0 +1,14 @@
+package com.adjh.springbootasync.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Please explain the class!!
+ *
+ * @author : jonghoon
+ * @fileName : AsyncController
+ * @since : 7/13/24
+ */
+@Controller
+public class AsyncController {
+}
