5a50a2ce83eb13780765ea8340de209e4e48188c
이종훈 <adjh54ir@gmail.com>
2024-07-17 18:00:48 +0900

    현재 구성중

---
+++ spring-boot-async/src/main/java/com/adjh/springbootasync/config/AsyncConfig.java
@@ -0,0 +1,16 @@
+package com.adjh.springbootasync.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Please explain the class!!
+ *
+ * @author : lee
+ * @fileName : AsyncConfig
+ * @since : 24. 7. 17.
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
