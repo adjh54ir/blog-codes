5fbcbcc33733991fc89f57d060da669f107541a0
jonghoon_lee <adjh54ir@gmail.com>
2024-08-18 23:14:57 +0900

    spring-boot-websocket room/workspace 구성중

--- react-chatting/src/routers/Routers.tsx
+++ react-chatting/src/routers/Routers.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import MainComponenet from '../components/MainComponent';
 import SocketIoComponent from '../components/socketio/SocketIoComponent';
+import SocketIoRoomComponet from '../components/socketio/SocketIoRoomComponet';
 
 const Routers = (props: any) => {
 	return (
@@ -10,6 +11,10 @@ const Routers = (props: any) => {
 				<Route path='/' element={<Navigate replace to='/main' {...props} />} />
 				<Route path={'main'} element={<MainComponenet {...props} />} />
 				<Route path={'socketIo'} element={<SocketIoComponent {...props} />} />
+				<Route
+					path={'socketIo/room'}
+					element={<SocketIoRoomComponet {...props} />}
+				/>
 			</Routes>
 		</Suspense>
 	);
