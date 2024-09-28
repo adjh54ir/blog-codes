b165e5c62cccb44025f0e2e38121df862c1a1727
jonghoon_lee <adjh54ir@gmail.com>
2024-09-28 16:43:04 +0900

    spring-boot-websocket 구성 완료

--- spring-boot-websocket/src/main/java/com/adjh/springbootwebsocket/controller/ChatController.java
+++ spring-boot-websocket/src/main/java/com/adjh/springbootwebsocket/controller/ChatController.java
@@ -1,14 +1,15 @@
 package com.adjh.springbootwebsocket.controller;
 
-import com.adjh.springbootwebsocket.dto.ChatMessage;
+import com.adjh.springbootwebsocket.dto.ChatMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Please explain the class!!
+ * WebSocket 데이터를 처리를 수행한 Controller입니다.
  *
  * @author : jonghoon
  * @fileName : ChatController
@@ -16,19 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ChatController {
-    private final SimpMessagingTemplate template;
+    private final SimpMessagingTemplate template;       // 특정 사용자에게 메시지를 보내는데 사용되는 STOMP을 이용한 템플릿입니다.
 
     @Autowired
     public ChatController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
+    /**
+     * Message 엔드포인트로 데이터와 함께 호출을 하면 "/sub/message"를 수신하는 사용자에게 메시지를 전달합니다.
+     *
+     * @param chatMessageDto
+     * @return
+     */
     @MessageMapping("/messages")
-    public ChatMessage send2(@RequestBody ChatMessage chatMessage) {
-        System.out.println("결과 값은 :: " + chatMessage.toString());
-//        template.convertAndSend("/sub/message", chatMessage.getContent());       // 구독중인 모든 사용자에게 메시지를 전달합니다.
-        System.out.println("결과 ::" + chatMessage.getContent());
-        return chatMessage;
+    public ChatMessageDto sendToMessage(@RequestBody ChatMessageDto chatMessageDto) {
+        template.convertAndSend("/sub/message", chatMessageDto);       // 구독중인 모든 사용자에게 메시지를 전달합니다.
+        return chatMessageDto;
     }
-
 }
