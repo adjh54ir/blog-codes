6c8b82af26db670c45b4e9de3d2565ee2770da48
jonghoon_lee <adjh54ir@gmail.com>
2024-10-01 13:13:23 +0900

    spring-boot3-security 초기환경 구성

---
+++ spring-boot3-security/src/main/java/com/adjh/springboot3security/SpringBoot3SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.adjh.springboot3security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SecurityApplication.class, args);
+    }
+
+}
