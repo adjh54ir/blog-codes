84ec774979a405dbac6fac2b5d283918d75587c6
jonghoon_lee <adjh54ir@gmail.com>
2024-10-01 13:13:23 +0900

    spring-boot3-security 초기환경 구성

---
+++ spring-boot3-security/src/test/java/com/adjh/springboot3security/SpringBoot3SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adjh.springboot3security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
