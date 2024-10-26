645ca1fc53d4124121040f3c5466f013e4c40cbc
jonghoon_lee <adjh54ir@gmail.com>
2024-10-26 16:34:07 +0900

    spring-boot-cors

---
+++ spring-boot-cors/src/main/java/com/adjh/springbootcors/SpringBootCorsApplication.java
@@ -0,0 +1,13 @@
+package com.adjh.springbootcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCorsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-cors/src/test/java/com/adjh/springbootcors/SpringBootCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adjh.springbootcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
