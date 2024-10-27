87abe5c46c6c1ce65d14e8b6aeffb1b07b68bea2
jonghoon_lee <adjh54ir@gmail.com>
2024-10-27 22:17:02 +0900

    spring-boot-security-oauth2 초기 Security 구성 완료

---
+++ spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/SpringBoot3SecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.adjh.springbootoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SecurityOauth2Application.class, args);
+    }
+
+}
