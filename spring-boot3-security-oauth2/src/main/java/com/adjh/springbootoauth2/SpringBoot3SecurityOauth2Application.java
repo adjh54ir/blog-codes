a0864d51121303047d939b7a63822b549762fdab
jonghoon_lee <adjh54ir@gmail.com>
2024-11-05 23:16:37 +0900

    spring-boot-properties 올리는중

--- spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/SpringBoot3SecurityOauth2Application.java
+++ spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/SpringBoot3SecurityOauth2Application.java
@@ -1,8 +1,12 @@
 package com.adjh.springbootoauth2;
 
+import com.adjh.springbootoauth2.dto.properties.OAuth2ClientProperties;
+import com.adjh.springbootoauth2.dto.properties.OAuth2ClientRecordProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({OAuth2ClientProperties.class, OAuth2ClientRecordProperties.class})
 @SpringBootApplication
 public class SpringBoot3SecurityOauth2Application {
 
