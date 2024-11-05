a47a6f09ec09dad4c9d3c212c04d5e72fd976333
adjh54ir <adjh54ir@gmail.com>
2024-11-05 10:02:53 +0900

    spring-boot3-security-oauth2 작업 내용 커밋

--- spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/controller/OAuth2Controller.java
+++ spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/controller/OAuth2Controller.java
@@ -1,13 +1,25 @@
 package com.adjh.springbootoauth2.controller;
 
 import com.adjh.springbootoauth2.dto.oauth2.LoginKakaoReqDto;
+import com.adjh.springbootoauth2.dto.oauth2.LoginNaverReqDto;
 import com.adjh.springbootoauth2.dto.oauth2.LoginNaverResDto;
 import com.adjh.springbootoauth2.service.OAuth2Service;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.introspector.PropertyUtils;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Please explain the class!!
@@ -30,6 +42,7 @@ public class OAuth2Controller {
      *
      * @param
      * @return ApiResponseWrapper<ApiResponse> : 응답 결과 및 응답 코드 반환
+     * @Refrence : https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#kakaologin
      */
     @GetMapping("/kakao")
     public ResponseEntity<Object> kakaoLogin(
@@ -55,11 +68,25 @@ public class OAuth2Controller {
      * [API] 네이버 로그인
      *
      * @return ApiResponseWrapper<ApiResponse> : 응답 결과 및 응답 코드 반환
+     * @refrence : https://developers.naver.com/docs/login/api/api.md
      */
-    @PostMapping("/naver")
-    public ResponseEntity<Object> naverLogin(@RequestBody LoginNaverResDto loginNaverResDto) {
+    @GetMapping("/naver")
+    public ResponseEntity<Object> naverLogin(
+            @RequestParam(required = false) String code,
+            @RequestParam(required = false) String error,
+            @RequestParam(required = false) String error_description,
+            @RequestParam(required = false) String state
+    ) {
+        LoginNaverReqDto naverReqDto = LoginNaverReqDto.builder()
+                .code(code)
+                .error(error)
+                .errorDescription(error_description)
+                .state(state)
+                .build();
 
-        return new ResponseEntity<>(loginNaverResDto, HttpStatus.OK);
+        Object resultObj = oAuth2Service.naverLogin(naverReqDto);
+
+        return new ResponseEntity<>(resultObj, HttpStatus.OK);
     }
 
 
--- spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/service/CustomOAuth2UserService.java
+++ spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/service/CustomOAuth2UserService.java
@@ -17,9 +17,8 @@ import org.springframework.stereotype.Service;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User user = super.loadUser(userRequest);
         // 여기서 사용자 정보를 처리하고 필요한 경우 데이터베이스에 저장
-        return user;
+        return super.loadUser(userRequest);
     }
 
 }
