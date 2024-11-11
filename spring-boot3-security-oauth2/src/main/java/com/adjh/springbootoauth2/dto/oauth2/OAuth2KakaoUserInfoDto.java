189f2c956a78a5d58eddf4d4703c4514bf2b6627
jonghoon_lee <adjh54ir@gmail.com>
2024-11-11 12:46:41 +0900

    spring-boot3-security-oauth2 카카오 개인정보 데이터 조회 추가

--- spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/dto/oauth2/OAuth2KakaoUserInfoDto.java
+++ spring-boot3-security-oauth2/src/main/java/com/adjh/springbootoauth2/dto/oauth2/OAuth2KakaoUserInfoDto.java
@@ -25,13 +25,26 @@ public class OAuth2KakaoUserInfoDto {
 
     private String thumbnailImageUrl;       // 썸네일 이미지 URL
 
+    private String name;                    // [Biz] 사용자 이름
+
+    private String ageRange;                // [Biz] 사용자 나이 범위
+
+    private String birthday;                // [Biz] 사용자 생일
+
+    private String gender;                  // [Biz] 사용자 성별
+
+
     @Builder
-    public OAuth2KakaoUserInfoDto(String id, int statusCode, String email, String nickname, String profileImageUrl, String thumbnailImageUrl) {
+    public OAuth2KakaoUserInfoDto(String id, int statusCode, String email, String nickname, String profileImageUrl, String thumbnailImageUrl, String name, String ageRange, String birthday, String gender) {
         this.id = id;
         this.statusCode = statusCode;
         this.email = email;
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
         this.thumbnailImageUrl = thumbnailImageUrl;
+        this.name = name;
+        this.ageRange = ageRange;
+        this.birthday = birthday;
+        this.gender = gender;
     }
 }
