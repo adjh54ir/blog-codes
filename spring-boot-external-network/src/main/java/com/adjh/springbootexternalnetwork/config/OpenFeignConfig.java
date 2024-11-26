73a6a22b25b749dcf30da55581cb243073de7426
adjh54ir <adjh54ir@gmail.com>
2024-11-26 17:29:11 +0900

    spring-boot-external-network 구성 완료

---
+++ spring-boot-external-network/src/main/java/com/adjh/springbootexternalnetwork/config/OpenFeignConfig.java
@@ -0,0 +1,35 @@
+package com.adjh.springbootexternalnetwork.config;
+
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenFeignConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("Global-Header", "global-value");
+            requestTemplate.header("Content-Type", "application/json");
+            requestTemplate.header("Accept", "application/json");
+        };
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        return new JacksonDecoder();
+    }
+
+    @Bean
+    public Encoder feignEncoder() {
+        return new JacksonEncoder();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
---
+++ spring-boot-external-network/src/main/java/com/adjh/springbootexternalnetwork/service/OpenFeignBusinessService.java
@@ -0,0 +1,40 @@
+package com.adjh.springbootexternalnetwork.service;
+
+import com.adjh.springbootexternalnetwork.dto.PostResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * OpenFeign 통신을 통해 가져온 응답 값을 통해 비즈니스 로직을 처리합니다.
+ */
+@Service
+public class OpenFeignBusinessService {
+    private final OpenFeignService openFeignService;
+
+    public OpenFeignBusinessService(OpenFeignService openFeignService) {
+        this.openFeignService = openFeignService;
+    }
+
+    // 특정 조건으로 게시물 필터링
+    public List<PostResponseDto> getFilteredPosts() {
+        List<PostResponseDto> posts = openFeignService.getPosts();
+        return posts.stream()
+                .filter(post -> post.getId() > 30)
+                .collect(Collectors.toList());
+    }
+
+    // 게시물 데이터 가공
+    public PostResponseDto getFilterBodySummary(String id) {
+        PostResponseDto post = openFeignService.getPostById(id);
+        // 컨텐츠 요약 추가
+        if (post.getBody().length() > 100) {
+            post.setBody(post.getBody().substring(0, 100) + "...");
+        } else {
+            post.setBody(post.getBody());
+        }
+        // 추가 데이터 처리 로직
+        return post;
+    }
+}
