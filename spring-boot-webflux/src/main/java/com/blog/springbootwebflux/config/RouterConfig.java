562686bbf96fac02fe81f4c4679d7124490a29d3
adjh54ir <adjh54ir@gmail.com>
2024-12-18 18:05:28 +0900

    spring-boot-webflux 구성중

--- spring-boot-webflux/src/main/java/com/blog/springbootwebflux/config/RouterConfig.java
+++ spring-boot-webflux/src/main/java/com/blog/springbootwebflux/config/RouterConfig.java
@@ -3,10 +3,11 @@ package com.blog.springbootwebflux.config;
 import com.blog.springbootwebflux.handler.UserHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
+import org.springframework.http.client.ReactorResourceFactory;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.netty.resources.LoopResources;
 
 /**
  * Please explain the class!!
@@ -17,6 +18,14 @@ import org.springframework.web.reactive.function.server.ServerResponse;
  */
 @Configuration
 public class RouterConfig {
+
+    @Bean
+    public ReactorResourceFactory reactorResourceFactory() {
+        ReactorResourceFactory factory = new ReactorResourceFactory();
+        factory.setUseGlobalResources(false); // 글로벌 리소스 사용 비활성화
+        factory.setLoopResources(LoopResources.create("event-loop-", 4, true));
+        return factory;
+    }
 
     /**
      * 사용자 라우터를 구성합니다.
