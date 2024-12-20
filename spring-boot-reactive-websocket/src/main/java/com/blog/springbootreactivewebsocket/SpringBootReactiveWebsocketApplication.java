a823e62426cf311dfd5c4dda3a26911d5045dc73
adjh54ir <adjh54ir@gmail.com>
2024-12-20 15:55:14 +0900

    spring-boot-reactive-websocket 구성 준비중

---
+++ spring-boot-reactive-websocket/src/main/java/com/blog/springbootreactivewebsocket/SpringBootReactiveWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.blog.springbootreactivewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveWebsocketApplication.class, args);
+    }
+
+}
