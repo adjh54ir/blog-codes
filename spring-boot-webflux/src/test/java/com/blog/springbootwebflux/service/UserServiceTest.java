234f81aca8d4263a83d5d83be34c94860e009eea
adjh54ir <adjh54ir@gmail.com>
2024-12-31 15:05:48 +0900

    spring-boot-webflux 테스트 추가

--- spring-boot-webflux/src/test/java/com/blog/springbootwebflux/service/UserServiceTest.java
+++ spring-boot-webflux/src/test/java/com/blog/springbootwebflux/service/UserServiceTest.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -23,6 +25,34 @@ import java.util.stream.IntStream;
 @SpringBootTest
 @AutoConfigureWebTestClient
 public class UserServiceTest {
+
+    @Test
+    public void testParallelProcessing() {
+        WebClient client = WebClient.create("http://localhost:8080");
+
+        // 10명의 서로 다른 사용자 데이터 생성
+        List<UserEntity> users = IntStream.range(0, 10)
+                .mapToObj(i -> UserEntity.builder()
+                        .userId("testIdd" + i)
+                        .userEmail("adjh54@naver.com")
+                        .build())
+                .collect(Collectors.toList());
+
+        // 각 사용자에 대해 테스트 실행
+        StepVerifier.create(
+                        Flux.fromIterable(users)
+                                .flatMap(user ->
+                                        client.get()
+                                                .uri("/api/v1/user/user/{userId}", user.getUserId())
+                                                .accept(MediaType.APPLICATION_JSON)
+                                                .exchange()
+                                                .thenReturn(Integer.class)
+                                                .elapsed()
+                                )
+                )
+                .expectNextCount(10)
+                .verifyComplete();
+    }
 
 
     @Test
