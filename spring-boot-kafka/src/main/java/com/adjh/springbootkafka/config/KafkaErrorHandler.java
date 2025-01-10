d478deb035ddcd3900e6ee31c6cf6213b89e9965
jonghoon_lee <adjh54ir@gmail.com>
2025-01-11 00:10:12 +0900

    spring-boot-kafka 1차 구성 완료

---
+++ spring-boot-kafka/src/main/java/com/adjh/springbootkafka/config/KafkaErrorHandler.java
@@ -0,0 +1,33 @@
+package com.adjh.springbootkafka.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+
+/**
+ * Please explain the class!!
+ *
+ * @author : jonghoon
+ * @fileName : KafkaErrorHandler
+ * @since : 25. 1. 10.
+ */
+@Slf4j
+@Configuration
+public class KafkaErrorHandler {
+    /**
+     * KafkaListenerErrorHandler 로 공통으로 사용하는 에러 핸들러를 구성합니다.
+     *
+     * @return
+     */
+    @Bean
+    public KafkaListenerErrorHandler customErrorHandler() {
+        return (message, exception) -> {
+            log.error("메시지 처리 중 오류 발생: {}", exception.getMessage());
+            log.error("문제가 발생한 메시지: {}", message.getPayload());
+            // 예외 처리 로직 구현
+            // 필요한 경우 다른 결과 반환 가능
+            return "Error Handled";
+        };
+    }
+}
---
+++ spring-boot-kafka/src/main/java/com/adjh/springbootkafka/service/KafkaProducerService.java
@@ -0,0 +1,70 @@
+package com.adjh.springbootkafka.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * kafkaTemplate 활용한 Topic 내에 메시지를 전송하는 다양한 방법
+ *
+ * @author : jonghoon
+ * @fileName : KafkaProducerService
+ * @since : 25. 1. 10.
+ */
+@Slf4j
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final String TOPIC_NAME = "example-topic";
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    /**
+     * 기본적인 메시지 전송
+     *
+     * @param message 전송 메시지
+     */
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC_NAME, message);
+    }
+
+    /**
+     * 키와 함께 메시지 전송
+     *
+     * @param key     Topic Key
+     * @param message 전송 메시지
+     */
+    public void sendMessageWithKey(String key, String message) {
+        kafkaTemplate.send(TOPIC_NAME, key, message);
+    }
+
+    /**
+     * 특정 파티션으로 메시지 전송
+     *
+     * @param message   전송 메시지
+     * @param partition 파티션 명
+     */
+    public void sendMessageToPartition(String message, int partition) {
+        kafkaTemplate.send(TOPIC_NAME, partition, null, message);
+    }
+
+
+    /**
+     * 비동기 전송 결과 처리
+     *
+     * @param message 전송 메시지
+     */
+    public void sendMessageWithCallback(String message) {
+        kafkaTemplate.send(TOPIC_NAME, message)
+                .whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        log.debug("Success: {} ", result.getRecordMetadata());
+                    } else {
+                        log.error("Failed: {}", ex.getMessage());
+                    }
+                });
+    }
+}
