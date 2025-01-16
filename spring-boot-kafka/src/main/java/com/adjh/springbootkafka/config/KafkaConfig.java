cb574d8c513e376b80fbbaecf164b2a2fe06256b
adjh54ir <adjh54ir@gmail.com>
2025-01-16 09:50:50 +0900

    jenkins pr 테스트

--- spring-boot-kafka/src/main/java/com/adjh/springbootkafka/config/KafkaConfig.java
+++ spring-boot-kafka/src/main/java/com/adjh/springbootkafka/config/KafkaConfig.java
@@ -39,7 +39,7 @@ public class KafkaConfig {
 
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);  
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
 
