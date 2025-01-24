1afd098ed74ac3357670f5a1bf7c2a2083c2cbf9
adjh54ir <adjh54ir@gmail.com>
2025-01-24 14:10:02 +0900

    spring-boot-keycloak 개발 환경 구축

---
+++ spring-boot-keycloak/src/test/java/com/blog/springbootkeycloak/SpringBootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
