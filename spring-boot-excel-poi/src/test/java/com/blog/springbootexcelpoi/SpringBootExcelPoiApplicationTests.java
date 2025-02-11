15f592fc565fb8081ea69aea91a9ca4e1abcf476
adjh54ir <adjh54ir@gmail.com>
2025-02-11 14:44:46 +0900

    spring-boot-excel-poi 초기 구성

---
+++ spring-boot-excel-poi/src/test/java/com/blog/springbootexcelpoi/SpringBootExcelPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.springbootexcelpoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExcelPoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
